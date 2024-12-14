337579839dea7b83705038b0e512f976e8b00e14
James Godwill Arkoh <jamesgodwillarkoh@gmail.com>
2024-12-14 10:23:48 +0000

    Trilo Front End Application setup of project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./styles/main.scss"; // Adjust the path based on your project structure
+
+function App() {
+  return (
+    <>
+      <div>
+        <p>Inside vite application</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
